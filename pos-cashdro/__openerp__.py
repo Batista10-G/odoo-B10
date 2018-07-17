af6160b9b0efe638bb4aa77de48bf8982656783d
Batista10 <batista10@marccasa.casa>
2018-07-17 09:28:56 +0200

    Afegir mòdul Cashdro

---
+++ pos-cashdro/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'pos-cashdro',
+    'summary': 'Cobrament amb Cashdro Odoo POS',
+    'author': 'Marc Tormo - Batista10',
+    'website': "https://www.batista10.cat",
+    'depends': ['pos'],
+    'version': '9.0.0.0.1',
+    'license': 'AGPL-3', 
+    'application': True,
+    'category': 'POS',
+    'data': ['views/pos-cashdro.xml',],
+}
