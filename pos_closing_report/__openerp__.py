869babac4e2f8ed014c17a641305b5a34b74931c
Batista10 - Marc Tormo i Bochaca <batista10.info@gmail.com>
2020-03-18 15:14:09 +0100

    filtres de dates. Assistent finalitzat

--- pos_closing_report/__openerp__.py
+++ pos_closing_report/__openerp__.py
@@ -5,7 +5,7 @@
     'author': "Batista10",
     'website': "https://www.batista10.cat",
     'category': 'Reports',
-    'version': '9.0.0.0.1',
+    'version': '9.0.0.0.2',
     'depends': ['base','report','point_of_sale'],
     'license': 'AGPL-3', 
     'application': True,
--- pos_closing_report/wizard/pos_wizard.py
+++ pos_closing_report/wizard/pos_wizard.py
@@ -5,18 +5,18 @@ from openerp import api, fields, models
 class posreportclosing(models.TransientModel):
     _name = 'pos.closing.model'
     _description = 'Wizard POS Closing Report'
-       
     config_id = fields.Many2one('pos.config', string='Terminal', required=True)
-
+    date_ini =  fields.Date(string='Date Start', requiered=True, default=fields.Date.today)
+    date_fi =  fields.Date(string='Date End', requiered=True, default=fields.Date.today)
 
 
     @api.multi
     def check_report(self):
         data = {}
-        data['form'] = self.read(['config_id'])[0]
+        data['form'] = self.read(['config_id','date_ini','date_fi'])[0]
         return self._print_report(data)
 
     def _print_report(self, data):
-        data['form'].update(self.read(['config_id'])[0])
+        data['form'].update(self.read(['config_id','date_ini','date_fi'])[0])
         return self.env['report'].get_action(self, 'pos_closing_report.pos_closing_report', data=data)
 
