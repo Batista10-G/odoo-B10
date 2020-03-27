046c43efc0e73fc2365e22a179e19e522c99bf42
Batista10 - Marc Tormo i Bochaca <batista10.info@gmail.com>
2020-03-27 08:53:41 +0100

    update for cov19

--- pos_closing_report/report/pos_closing_report_wizard.py
+++ pos_closing_report/report/pos_closing_report_wizard.py
@@ -3,13 +3,17 @@ from openerp import api, models
 from openerp.exceptions import UserError
 
 
-class ReportHelloworld(models.AbstractModel):
-    _name = 'report.pos_closing_report.pos_closing_report'
+class Reportposreportclosing(models.AbstractModel):
+    _name = 'pos.closing.model'
+
+    wizard_id = fields.Many2one('pos.closing.wizard.model')
+    session_id = fields.Many2many('pos.session')
 
     @api.model
     def render_html(self, docids, data=None):
         self.model = self.env.context.get('active_model')
         docs = self.env[self.model].browse(self.env.context.get('active_id'))
+        
         
         docargs = {
             'doc_ids': self.ids,
--- pos_closing_report/wizard/pos_wizard.py
+++ pos_closing_report/wizard/pos_wizard.py
@@ -3,12 +3,11 @@
 from openerp import api, fields, models
 
 class posreportclosing(models.TransientModel):
-    _name = 'pos.closing.model'
+    _name = 'pos.closing.wizard.model'
     _description = 'Wizard POS Closing Report'
     config_id = fields.Many2one('pos.config', string='Terminal', required=True)
     date_ini =  fields.Date(string='Date Start', requiered=True, default=fields.Date.today)
     date_fi =  fields.Date(string='Date End', requiered=True, default=fields.Date.today)
-
 
     @api.multi
     def check_report(self):
@@ -17,6 +16,6 @@ class posreportclosing(models.TransientModel):
         return self._print_report(data)
 
     def _print_report(self, data):
-        data['form'].update(self.read(['config_id','date_ini','date_fi'])[0])
+        data['form'].update(self.read(['config_id','date_ini','date_fi','session_id'])[0])
         return self.env['report'].get_action(self, 'pos_closing_report.pos_closing_report', data=data)
 
