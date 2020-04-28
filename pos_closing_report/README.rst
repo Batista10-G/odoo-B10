70dd22b8fab6b36a791cb7cea24d4a13d93a48c6
Batista10 Serveis informàtics <batista10.info@gmail.com>
2020-04-28 19:53:00 +0200

    Versió inicial

--- pos_closing_report/README.rst
+++ pos_closing_report/README.rst
@@ -5,6 +5,6 @@ Aquest mòdul crea un informe de resum dels tancaments de caixa diaris.
 
 Registre de Versions:
 
-- 10.0.0.1.0
+- 11.0.0.0.0
 
-* Traspàs des de versió 9
+* Traspàs des de versió 10
--- pos_closing_report/__manifest__.py
+++ pos_closing_report/__manifest__.py
@@ -5,7 +5,7 @@
     'author': "Batista10",
     'website': "https://www.batista10.cat",
     'category': 'Reports',
-    'version': '10.0.0.1.0',
+    'version': '11.0.0.0.0',
     'depends': ['base','report','point_of_sale'],
     'license': 'AGPL-3', 
     'application': True,
